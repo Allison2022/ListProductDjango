3183cf3a78f1f6dabdd0acc0eefec5b52d8a179e
Allison H <eherazo@lsv-tech.com>
2023-02-13 12:42:14 -0500

    appListProduct

--- listelement/admin.py
+++ listelement/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+from listelement.models import Category, Type, Element
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('title', 'url_clean')
+
+@admin.register(Type)
+class TypeAdmin(admin.ModelAdmin):
+    list_display = ('title', 'url_clean')
+
+@admin.register(Element)
+class ElementAdmin(admin.ModelAdmin):
+    list_display = ('title', 'url_clean', 'description', 'category', 'type')
