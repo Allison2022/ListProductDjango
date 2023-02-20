3181a278ddd1d9e5f87d7c30ca5959db5fbe99cd
Allison H <eherazo@lsv-tech.com>
2023-02-20 12:53:41 -0500

    creacion vista update

---
+++ comment/forms.py
@@ -0,0 +1,14 @@
+from django.forms import ModelForm, Textarea
+from .models import Comment
+
+class CommentForm(ModelForm):
+    class Meta:
+        model = Comment
+        fields = ('text',)
+        widgets = {
+            'text':Textarea(attrs={'class':'form-input'})
+        }
+
+        '''def __init__(self, *args, **kwargs):
+            super().__init__(*args, **kwargs)
+            self.fields['text'].widget.attrs.update({'class':'form-input'})        '''
--- comment/views.py
+++ comment/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Comment
+from .forms import CommentForm
 
 # index view
 def index(request):
@@ -8,3 +9,27 @@ def index(request):
         'comments':comments
     }
     return render(request, 'index.html', response)
+
+# form view
+def add(request):
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            form.save(commit=True)
+            return redirect('index')
+    else:
+        form = CommentForm()
+    return render(request, 'add.html', {'form':form})
+
+# form view update
+def update(request, pk):
+
+    comment = get_object_or_404(Comment, pk=pk)
+    if request.method == 'POST':
+        form = CommentForm(request.POST, instance=comment)
+        if form.is_valid():
+            form.save()
+            return redirect('update', pk=comment.id)
+    else:
+        form = CommentForm(instance=comment)
+    return render(request, 'update.html', {'form':form, 'comment':comment})
